1093277bf8c3473ed4ceb08399eabf3558c3f4b7
Eduard Ridilla <eduard.ridilla@student.tuke.sk>
2021-11-04 16:57:59 +0100

    initial commit

---
+++ BPSmartdata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BPSmartdata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
