13f7134e5acb55db477fbae8f3740ec9f65fd136
Eduard Ridilla <eduard.ridilla@student.tuke.sk>
2021-11-05 09:59:29 +0100

    getting win version test

--- BPSmartdata/Heartbeat.cs
+++ BPSmartdata/Heartbeat.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Net;
 using System.Timers;
+using System.Management;
 
 namespace BPSmartdata
 {
@@ -17,7 +18,7 @@ namespace BPSmartdata
 
         private void TimerElapsed(object sender, ElapsedEventArgs e)
         {
-            string[] lines = new string[] {DateTime.Now.ToString()};
+            string[] lines = new string[] {DateTime.Now.ToString(), GetWinVersion()};
             File.AppendAllLines(@"E:\TUKE\Rider\BPSmartdata\BPSmartdata\out.txt", lines);
         }
 
@@ -30,5 +31,36 @@ namespace BPSmartdata
         {
             _timer.Stop();
         }
+
+        private String GetWinVersion()
+        {
+            String result = "Unable to retrieve Windows version.";
+            ManagementScope scope = null;
+
+            try
+            {
+                ConnectionOptions options = new ConnectionOptions();
+                options.Impersonation = System.Management.ImpersonationLevel.Impersonate;
+
+                scope = new ManagementScope("\\\\eddie\\root\\cimv2", options);
+                scope.Connect();
+
+                ObjectQuery query = new ObjectQuery("Select Caption FROM win32_OperatingSystem");
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+                ManagementObjectCollection queryCollection = searcher.Get();
+                foreach (ManagementObject o in queryCollection)
+                {
+                    result = o["Caption"].ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return result;
+        } 
     }
 }
